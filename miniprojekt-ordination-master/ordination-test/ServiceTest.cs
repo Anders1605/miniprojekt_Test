8ee1cedb55d0396ca9f4db7e2c1523ca8f816d72
Jacob Lageri <jacob.Lageri@hotmail.com>
2025-12-03 17:05:30 +0100

    Added OpretPNTest in ServiceTest

--- miniprojekt-ordination-master/ordination-test/ServiceTest.cs
+++ miniprojekt-ordination-master/ordination-test/ServiceTest.cs
@@ -9,71 +9,93 @@ using shared.Model;
 [TestClass]
 public class ServiceTest
 {
-    private DataService service;
+	private DataService service;
 
-    [TestInitialize]
-    public void SetupBeforeEachTest()
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
-        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
-        var context = new OrdinationContext(optionsBuilder.Options);
-        service = new DataService(context);
-        service.SeedData();
-    }
+	[TestInitialize]
+	public void SetupBeforeEachTest()
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+		optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+		var context = new OrdinationContext(optionsBuilder.Options);
+		service = new DataService(context);
+		service.SeedData();
+	}
 
-    [TestMethod]
-    public void PatientsExist()
-    {
-        Assert.IsNotNull(service.GetPatienter());
-    }
+	[TestMethod]
+	public void PatientsExist()
+	{
+		Assert.IsNotNull(service.GetPatienter());
+	}
 
-    [TestMethod]
-    public void OpretDagligFast()
-    {
-        Patient patient = service.GetPatienter().First();
-        Laegemiddel lm = service.GetLaegemidler().First();
+	[TestMethod]
+	public void OpretDagligFast()
+	{
+		Patient patient = service.GetPatienter().First();
+		Laegemiddel lm = service.GetLaegemidler().First();
 
-        Assert.AreEqual(1, service.GetDagligFaste().Count());
+		Assert.AreEqual(1, service.GetDagligFaste().Count());
 
-        service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
-            2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+		service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
+			2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
 
-        Assert.AreEqual(2, service.GetDagligFaste().Count());
-    }
+		Assert.AreEqual(2, service.GetDagligFaste().Count());
+	}
 
-    [TestMethod]
-    [ExpectedException(typeof(InvalidOperationException))]
-    public void TestAtKodenSmiderEnException()
-    {
+	[TestMethod]
+	[ExpectedException(typeof(InvalidOperationException))]
+	public void TestAtKodenSmiderEnException()
+	{
 		var patienter = service.GetPatienter();
 		var lægemidler = service.GetLaegemidler();
 		var ordinationer = service.GetPNs();
 
-        patienter[0].PatientId = 20;
+		patienter[0].PatientId = 20;
 
 		//Act
 
 		var PNToTest = service.OpretPN(patienter[0].PatientId, lægemidler[0].LaegemiddelId, 10, DateTime.Now.AddDays(-5), DateTime.Now.AddDays(5));
 
 		Console.WriteLine("Her kommer der ikke en exception. Testen fejler.");
-    }
+	}
 
-    [TestMethod]
-    public void AvendOrdinationTestSuccess()
-    {
+	[TestMethod]
+	public void AvendOrdinationTestSuccess()
+	{
 
-        //Arrange
-        var patienter = service.GetPatienter();
-        var lægemidler = service.GetLaegemidler();
-        var ordinationer = service.GetPNs();
+		//Arrange
+		var patienter = service.GetPatienter();
+		var lægemidler = service.GetLaegemidler();
+		var ordinationer = service.GetPNs();
 
 
-        //Act
+		//Act
 
-        var PNToTest = service.OpretPN(patienter[0].PatientId, lægemidler[0].LaegemiddelId, 10, DateTime.Now.AddDays(-5), DateTime.Now.AddDays(5));
-       var result = service.AnvendOrdination(PNToTest.OrdinationId, new Dato() { dato = DateTime.Now});
-        
-        //Assert
-        Assert.AreEqual("Ordination anvendt.", result);
-    }
+		var PNToTest = service.OpretPN(patienter[0].PatientId, lægemidler[0].LaegemiddelId, 10, DateTime.Now.AddDays(-5), DateTime.Now.AddDays(5));
+		var result = service.AnvendOrdination(PNToTest.OrdinationId, new Dato() { dato = DateTime.Now });
+
+		//Assert
+		Assert.AreEqual("Ordination anvendt.", result);
+	}
+
+	[TestMethod]
+	public void OpretPNTest()
+	{
+
+		//Arrange
+		var patienter = service.GetPatienter();
+		var lægemidler = service.GetLaegemidler();
+		var ordinationer = service.GetPNs();
+
+		int forventet = ordinationer.Count + 1;
+
+		//Act
+
+		var PNToTest = service.OpretPN(patienter[0].PatientId, lægemidler[0].LaegemiddelId, 10, DateTime.Now.AddDays(-5), DateTime.Now.AddDays(5));
+
+		ordinationer = service.GetPNs();
+
+		//Assert
+
+		Assert.AreEqual(forventet, ordinationer.Count);
+	}
 }
